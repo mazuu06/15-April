1b80c93ab5ca66b35a80d9d827f574003451c481
RaNa MazHar Naseer <103133005+Hawk-06@users.noreply.github.com>
2022-04-15 22:36:15 +0500

    Add files via upload

---
+++ 15_April.py
@@ -0,0 +1,105 @@
+###################################### Arithmetic Operators ####################################################
+
+# x = 12
+# y = 5
+
+# print("Addition : ",x+y)
+# print("Subtraction : ",x-y)
+# print("Multiplication : ",x*y)
+# print("Division : ",x/y)
+# print("Modulus : ",x%y)
+# print("Exponentiation : ",x**y)
+# print("Floor division : ",x//y)
+
+###################################### Comparison Operators ####################################################
+
+# x = 5
+# y = 7
+
+# print("Greater than : ",x>y)
+# print("Less than : ",x<y)
+# print("Greater than or equal to : ",x>=y)
+# print("Less than or equal to : ",x<=y)
+# print("Not equal : ",x!=y)
+# print("Equal : ",x==y)
+
+##################################### Logical Operators ####################################################
+
+# x = 5
+# y = 8
+
+# print("and : ",x<10 and y>10)
+# print("or : ",x>2 or y<20)
+# print("not : ",not(x > 4))
+
+##################################### Bitwise Operators ####################################################
+
+# x = 27
+# y = 12
+
+# print("Bitwise AND : ",x&y)
+# print("Bitwise OR : ",x|y)
+# print("Bitwise NOT : ",~x)
+# print("Bitwise XOR : ",x^y)
+# print("Bitwise right shift : ",x>>1)
+# print("Bitwise left shift",x<<2)
+
+##################################### Assignment Operators ####################################################
+
+x = 13
+
+# x+=3
+# print(x)
+
+# x-=3
+# print(x)
+
+# x*=3
+# print(x)
+
+# x/=2
+# print(x)
+
+# x%=3
+# print(x)
+
+# x//=7
+# print(x)
+
+# x**=4
+# print(x)
+
+# x&=3
+# print(x)
+
+# x|=4
+# print(x)
+
+# x^=4
+# print(x)
+
+# x>>=2
+# print(x)
+
+# x<<=2
+# print(x)
+
+##################################### Identity Operators ####################################################
+
+# x = 12
+# y = x
+# z = str(12)
+
+# print(x is y)
+# print(x is z)
+# print(x is not y)
+# print(x is not z)
+
+##################################### Membership Operators ####################################################
+
+lst = [1,5,8,7,12,6,9]
+
+print(2 in lst)
+print(2 not in lst)
+print(9 in lst)
+print(9 not in lst)
